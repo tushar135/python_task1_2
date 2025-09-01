97b5de556a9c8217f54b6bc7b5c6ee3613ebbfe8
tushar135 <71786768+tushar135@users.noreply.github.com>
2025-09-01 14:07:51 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,12 @@
+a=int(input("enter the first number: "))
+b=int(input("enter the second number: "))
+
+addition=a+b
+substraction=a-b
+multiplication=a*b
+division=a/b
+
+print("the addition of two numbers is: ",addition)
+print("the substraction of two numbers is: ",substraction)
+print("the multiplication of two numbers is: ",multiplication)  
+print("the division of two numbers is: ",division)
---
+++ task2.py
@@ -0,0 +1,5 @@
+first_name=input("enter your first name: ")
+last_name=input("enter your last name: ")
+
+full_name=first_name+ " " +last_name
+print("hello," +full_name+ ",welcome to python programming")
